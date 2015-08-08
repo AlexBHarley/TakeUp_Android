ee01d236179ac609a5e27d4bae1918ec96aded26
CraigWilkinson <craiginatorw@gmail.com>
2015-08-08 14:28:56 +1200

    Craig's test for my Hobby page and list

---
+++ app/src/main/java/sot/hobbyapp/HobbyActivity.java
@@ -0,0 +1,25 @@
+package sot.hobbyapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Craig on 8/08/2015.
+ */
+public class HobbyActivity extends Activity{
+    TextView hobbyName;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_hobby);
+        super.onCreate(savedInstanceState);
+        Intent hobby_name = getIntent();
+        hobbyName.setText(hobby_name.getStringExtra("hobby_name"))
+
+
+    }
+}
