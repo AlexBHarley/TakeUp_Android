7dd7c03ae33a097dce4c03391731ccb4c28b5a11
Alex <alexbruceharley@gmail.com>
2015-08-08 14:21:29 +1200

    Hobby objects

--- app/src/main/java/sot/hobbyapp/HobbyActivity.java
+++ app/src/main/java/sot/hobbyapp/HobbyActivity.java
@@ -1,32 +1,35 @@
 package sot.hobbyapp;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.widget.Toast;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
 
-
-public class MainActivity extends Activity {
-
+/**
+ * Created by alex on 8/08/15.
+ */
+public class HobbyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         HttpClient httpClient = new DefaultHttpClient();
 
         HttpGet httpget = new HttpGet("http://api.trademe.co.nz/v1/Search/General.xml?search_string=iphone");
         httpget.addHeader("auth",
-                "Authorization: OAuth oauth_consumer_key=CE371CEC569880E6413594C1E27B6ADA" +
+                          "Authorization: OAuth oauth_consumer_key=CE371CEC569880E6413594C1E27B6ADA" +
                         ", oauth_signature_method=PLAINTEXT, oauth_signature=38F2375F4D3324E9AFAE548603341F26&"
         );
         HttpResponse response;
@@ -39,28 +42,5 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/sot/hobbyapp/HobbyClass.java
@@ -0,0 +1,17 @@
+package sot.hobbyapp;
+
+/**
+ * Created by alex on 8/08/15.
+ */
+public class HobbyClass {
+    private String name;
+    private HobbyObject[] items;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
