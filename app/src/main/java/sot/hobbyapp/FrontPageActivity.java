382cfe96017b4ad12766e4c623effb6f8a95de35
Nivlagx <galvin9@hotmail.com>
2015-08-08 14:10:45 +1200

    Background color test

--- app/src/main/java/sot/hobbyapp/FrontPageActivity.java
+++ app/src/main/java/sot/hobbyapp/FrontPageActivity.java
@@ -1,9 +1,11 @@
 package sot.hobbyapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class FrontPageActivity extends Activity {
 
@@ -11,6 +13,11 @@ public class FrontPageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
+
+        //Layout
+        RelativeLayout testLayout = new RelativeLayout(this);
+        testLayout.setBackgroundColor(Color.GREEN);
+
     }
 
     @Override
